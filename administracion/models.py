380e3f734eec01940caaf8d691cd2cf6c56b0641
Alexis <aogdad@gmail.com>
2014-04-14 12:27:20 -0400

    creando proyecto

--- administracion/models.py
+++ administracion/models.py
@@ -4,8 +4,23 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+"""
+    Se utiliza la clase User por de defecto en Django para la creacion de los usuarios, pero
+    se incluyen 3 atributos:
+        - direccion: direccion del usuario
+        - telefono: numero del telefono del usuario
+        - observacion: observacion sobre el usuario por parte del administrador del sistema
+
+
+"""
+
 User.add_to_class('direccion', models.FloatField(null=True, blank=True))
 User.add_to_class('telefono', models.PositiveIntegerField(null=True, blank=True))
 User.add_to_class('observacion', models.PositiveIntegerField(null=True, blank=True))
 
 
+class Proyecto(models.Model):
+    Nombre_del_Proyecto = models.CharField(max_length=30, unique=True)
+    Descripcion_del_Proyecto= models.TextField
+    Fecha_de_inicio = models.DateField
+    Fecha_de_finalizacion = models.DateField
