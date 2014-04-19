ae4bc68b15ca9f64a966c4b49b6524df4d25651f
RodrigoFe <rodri93enma@gmail.com>
2014-04-18 22:17:21 -0400

    los roles por defecto han sido agregados

--- administracion/models.py
+++ administracion/models.py
@@ -1,6 +1,6 @@
 
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group, Permission
 
 # Create your models here.
 """
@@ -32,3 +32,51 @@ class Fase(models.Model):
 
     def __unicode__(self):
         return self.Nombre
+
+def tienePermiso(self, permiso):
+    for grupo in self.groups.all():
+        for permisoUsuario in grupo.permissions.all():
+            if permisoUsuario.codename == permiso:
+                return True
+    return False
+
+User.add_to_class('tienePermiso', tienePermiso)
+
+def accesoAdministracion(self):
+    rol ='Administracion'
+    for grupo in self.groups.all():
+        if grupo.name == rol:
+            return True
+    return False
+
+User.add_to_class('accesoAdministracion', accesoAdministracion)
+
+#Rol administracion
+grupoAdministracion, created = Group.objects.get_or_create(name='Administracion')
+if created:
+    grupoAdministracion.save()
+    #permisos de Proyecto
+permisoAdd = Permission.objects.get(codename='add_proyecto')
+permisoChange = Permission.objects.get(codename='change_proyecto')
+permisoDelete = Permission.objects.get(codename='delete_proyecto')
+grupoAdministracion.permissions.add(permisoAdd, permisoChange, permisoDelete)
+    #permisos de Usuario
+permisoAdd = Permission.objects.get(codename='add_user')
+permisoChange = Permission.objects.get(codename='change_user')
+permisoDelete = Permission.objects.get(codename='delete_user')
+grupoAdministracion.permissions.add(permisoAdd, permisoChange, permisoDelete)
+    #permisos de Roles
+permisoAdd = Permission.objects.get(codename='add_group')
+permisoChange = Permission.objects.get(codename='change_group')
+permisoDelete = Permission.objects.get(codename='delete_group')
+grupoAdministracion.permissions.add(permisoAdd, permisoChange, permisoDelete)
+
+
+    #Rol lider de Proyecto
+grupoLiderProyecto, created = Group.objects.get_or_create(name='Lider de Proyecto')
+if created:
+    grupoLiderProyecto.save()
+permisoAdd = Permission.objects.get(codename='add_fase')
+permisoChange = Permission.objects.get(codename='change_fase')
+permisoDelete = Permission.objects.get(codename='delete_fase')
+grupoLiderProyecto.permissions.add(permisoAdd, permisoChange, permisoDelete)
