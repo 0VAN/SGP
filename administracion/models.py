f434f699f0be4f9b6d022abc5acb7b2e0502a96c
RodrigoFe <rodri93enma@gmail.com>
2014-04-20 14:41:18 -0400

    agregados los campos al modelo Group para ragistrar usuario creador y fecha creacion

--- administracion/models.py
+++ administracion/models.py
@@ -17,6 +17,7 @@ User.add_to_class('direccion', models.TextField(null=True, blank=True))
 User.add_to_class('telefono', models.PositiveIntegerField(null=True, blank=True))
 User.add_to_class('observacion', models.TextField(null=True, blank=True))
 Group.add_to_class('Usuario', models.ForeignKey(User, null=True))
+Group.add_to_class('Fecha', models.DateTimeField(auto_now=True, null=True))
 
 
 class Proyecto(models.Model):
