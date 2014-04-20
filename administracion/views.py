3fdbadc8f409254a15226bfec5af507831c5b4fd
Juan Francisco Figueredo <juanffigueredo@gmail.com>
2014-04-20 16:45:29 -0400

    Mensajes de exito

--- administracion/views.py
+++ administracion/views.py
@@ -86,7 +86,8 @@ def crear_usuario(request):
         formulario = UsuarioForm(request.POST)
         if formulario.is_valid():
             formulario.save()
-            return render_to_response('usuario/operacion_usuario_exito.html', {'mensaje': 'Usuario creado con exito'}
+            lista_usuarios = User.objects.all()
+            return render_to_response('usuario/operacion_usuario_exito.html', {'mensaje': 'Usuario creado con exito', 'usuario_admin': usuario, 'lista_usuarios': lista_usuarios}
                                       , context_instance=RequestContext(request))
     else:
         formulario = UsuarioForm()
@@ -105,8 +106,9 @@ def modificar_usuario(request):
            form = formulario.save(commit=False)
            form.user = request
            form.save()
+           lista_usuarios = User.objects.all()
            return render_to_response('usuario/operacion_usuario_exito.html',
-                                     {'mensaje': 'Usuario modificado con exito'},
+                                     {'mensaje': 'Usuario modificado con exito', 'usuario_admin': usuario, 'lista_usuarios': lista_usuarios},
                                      context_instance=RequestContext(request))
     else:
         formulario = UsuarioModForm(instance=usuario)
@@ -118,12 +120,14 @@ def modificar_usuario(request):
 @user_passes_test( User.can_change_user , login_url="/iniciar_sesion")
 def cambioEstado_usuario_form(request, id_usuario):
     usuarioDetalle = User.objects.get(pk=id_usuario)
+    usuario = request.user
     if request.method == 'POST':
         formulario = UsuarioDelForm(request.POST, instance=usuarioDetalle)
         if formulario.is_valid():
            formulario.save()
+           lista_usuarios = User.objects.all()
            return render_to_response('usuario/operacion_usuario_exito.html',
-                                     {'mensaje':'Cambio de estado de usuario con exito'}
+                                     {'mensaje':'Cambio de estado de usuario con exito', 'usuario_admin':usuario, 'lista_usuarios':lista_usuarios}
                                      , context_instance=RequestContext(request))
     else:
         formulario = UsuarioDelForm(instance=usuarioDetalle)
@@ -155,7 +159,7 @@ def nuevo_proyecto(request):
         if formulario.is_valid():
             formulario.save()
             return render_to_response('proyecto/crear_proyecto_exito.html',
-                                      {'mensaje':'Proyecto creado con exito', 'usuario':request.user,
+                                      {'mensaje':'Proyecto creado con exito', 'usuarioProyecto':request.user,
                                        'lista_proyectos':lista_proyectos},
                                       context_instance=RequestContext(request))
     else:
@@ -247,7 +251,8 @@ def crear_rol(request):
         formulario = RolForm(request.POST)
         if formulario.is_valid():
             formulario.save()
-            return render_to_response('rol/crear_rol_exito.html', {'mensaje':mensaje,'usuario':usuario},context_instance=RequestContext(request))
+            roles = Group.objects.all()
+            return render_to_response('rol/crear_rol_exito.html', {'mensaje':mensaje,'usuario':usuario, 'roles': roles},context_instance=RequestContext(request))
     else:
         formulario = RolForm()
     return render_to_response('rol/crear_rol.html', {'formulario':formulario, 'usuario':usuario},context_instance=RequestContext(request))
@@ -280,7 +285,9 @@ def vista_eliminar_rol(request, idRol):
 def eliminar_rol(request, idRol):
     rol = Group.objects.get(pk=idRol)
     rol.delete()
-    return render_to_response('rol/roleliminado.html',context_instance=RequestContext(request))
+    usuario = request.user
+    roles = Group.objects.all()
+    return render_to_response('rol/roleliminado.html', {'usuario': usuario, 'roles':roles}, context_instance=RequestContext(request))
 
 @user_passes_test( User.can_change_user , login_url="/iniciar_sesion")
 def vista_asignar_rol(request):
@@ -296,8 +303,9 @@ def asignar_rol(request, idRol):
         formulario = AsignarRol(request.POST, instance=usuario)
         if formulario.is_valid():
            formulario.save()
+           roles = Group.objects.all()
            return render_to_response('rol/operacion_rol_exito.html',
-                                     {'mensaje': 'La operacion ha sido exitosa!'},
+                                     {'mensaje': 'Rol asignado con exito', 'usuario': usuario_logueado, 'roles': roles},
                                      context_instance=RequestContext(request))
     else:
         formulario = AsignarRol(instance=usuario)
