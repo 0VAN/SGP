36b63f62b20d7a3c5b35077a6b1edac00db0061a
RodrigoFe <rodri93enma@gmail.com>
2014-05-21 21:55:52 -0400

    anhadiendo comite de gestion

--- gestion/models.py
+++ gestion/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from desarrollo.models import *
+from administracion.models import *
 # Create your models here.
 
 class LineBase(models.Model):
@@ -8,3 +9,9 @@ class LineBase(models.Model):
     Usuario = models.ForeignKey(User)
     Items = models.ManyToManyField(Item)
     Fase = models.ForeignKey(Fase)
+
+class ComiteDeCambio(models.Model):
+    Usuario1 = models.ForeignKey(User, related_name="Usuario1")
+    Usuario2 = models.ForeignKey(User, related_name="Usuario2")
+    Usuario3 = models.ForeignKey(User, related_name="Usuario3")
+    Proyecto = models.ForeignKey(Proyecto)
